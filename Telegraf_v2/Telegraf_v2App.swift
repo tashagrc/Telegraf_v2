6ec49ec7f3796c0c7b0fb2d5e4de50d8e52faf20
Natasha Radika <81007696+tashagrc@users.noreply.github.com>
2024-05-23 09:55:30 +0700

    Initial Commit

---
+++ Telegraf_v2/Telegraf_v2App.swift
@@ -0,0 +1,17 @@
+//
+//  Telegraf_v2App.swift
+//  Telegraf_v2
+//
+//  Created by Natasha Radika on 23/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Telegraf_v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Telegraf_v2UITests/Telegraf_v2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Telegraf_v2UITestsLaunchTests.swift
+//  Telegraf_v2UITests
+//
+//  Created by Natasha Radika on 23/05/24.
+//
+
+import XCTest
+
+final class Telegraf_v2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
