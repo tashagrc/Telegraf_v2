3d44d37744368ffe1d37d6bb075e74aee640bfed
Natasha Radika <81007696+tashagrc@users.noreply.github.com>
2024-05-25 23:36:33 +0700

    made empty page for onboarding

---
+++ Telegraf_v2/View/Onboard/GestureSuccessView.swift
@@ -0,0 +1,18 @@
+//
+//  GestureSuccessView.swift
+//  Telegraf_v2
+//
+//  Created by Natasha Radika on 25/05/24.
+//
+
+import SwiftUI
+
+struct GestureSuccessView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GestureSuccessView()
+}
---
+++ Telegraf_v2/View/Onboard/OnboardDeviceView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardDeviceView.swift
+//  Telegraf_v2
+//
+//  Created by Natasha Radika on 25/05/24.
+//
+
+import SwiftUI
+
+struct OnboardDeviceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardDeviceView()
+}
